1204688fb44ec4ffca70b26cc41b009d50967a0c
forlav123 <forlavhadiman@gmail.com>
2026-06-24 20:51:30 +0700

    Update title to Sistem Pemesanan Barang Online

--- resources/views/layout_final.blade.php
+++ resources/views/layout_final.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Toko Maju Jaya - Solusi Belanja Gadget & Fashion Premium')</title>
+    <title>@yield('title', 'Toko Maju Jaya - Sistem Pemesanan Barang Online')</title>
     <!-- Font Awesome CDN -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!-- Main Style CSS -->
